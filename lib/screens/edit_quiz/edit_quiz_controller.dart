26f57ea6e699cc197d17e64b8ba7fffea5b9ca15
arnaumarti <arnaums02@gmail.com>
2025-04-26 18:04:52 +0200

    only to prove

--- lib/screens/edit_quiz/edit_quiz_controller.dart
+++ lib/screens/edit_quiz/edit_quiz_controller.dart
@@ -1,4 +1,5 @@
 // Puedes agregar la lógica para editar el cuestionario aquí
 class EditQuizController {
   // Lógica para editar el cuestionario
+  // gio gio, tinc un mètode infalible, fica text per centrar
 }
